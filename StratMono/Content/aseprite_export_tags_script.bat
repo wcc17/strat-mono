026a35d078d5a25cc71dafdebe1249282453e2ce
wcc17 <christian.curry17@gmail.com>
2021-09-18 18:00:04 -0400

    initial commit

---
+++ StratMono/Content/aseprite_export_tags_script.bat
@@ -0,0 +1,17 @@
+@echo off
+
+cd assets
+rmdir /Q /S sprites
+mkdir sprites
+
+
+for %%f in (./raw/*) do (
+    Rem f is the path, ~nf is the name of the file
+    echo %%f
+    echo %%~nf
+
+    Aseprite.exe -b raw/%%f --save-as "sprites/%%~nf_{tag}/%%~nf_{tag}.png"
+)
+
+cd ../
+mono ..\..\..\..\Nez\Nez.SpriteAtlasPacker\PrebuiltExecutable\SpriteAtlasPacker.exe -image:roots.png -map:roots.atlas ./assets/sprites

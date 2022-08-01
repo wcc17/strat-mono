5f240dad19a444ae768215817123e05048438cb6
wcc17 <christian.curry17@gmail.com>
2022-08-01 00:29:04 -0400

    improve and add controller controls to cursor movement

--- StratMono/Scenes/LevelScene.cs
+++ StratMono/Scenes/LevelScene.cs
@@ -6,6 +6,7 @@ using Nez.Sprites;
 using StratMono.Components;
 using StratMono.Entities;
 using StratMono.System;
+using System.Timers;
 
 namespace StratMono.Scenes
 {
@@ -23,6 +24,12 @@ namespace StratMono.Scenes
 
         private SpriteAtlas _spriteAtlas;
         private GridSystem _gridSystem;
+
+        private VirtualIntegerAxis _cursorMovementXAxisInput;
+        private VirtualIntegerAxis _cursorMovementYAxisInput;
+        private Vector2 _cursorMovementDirection = new Vector2(0, 0);
+        private Timer _timer = new Timer();
+        private bool _disableCursorControllerMovement;
 
         public override void Initialize()
         {
@@ -53,9 +60,93 @@ namespace StratMono.Scenes
 
             // TODO: system?
             var cursorEntity = FindEntity(CursorEntityName);
-            cursorEntity.Position = new Vector2(
-                Input.MousePosition.X + (Camera.Position.X - Screen.Width / 2),
-                Input.MousePosition.Y + (Camera.Position.Y - Screen.Height / 2));
+
+            if (Input.MousePositionDelta.X > 0 || Input.MousePositionDelta.Y > 0)
+            {
+                cursorEntity.Position = new Vector2(
+                    Input.MousePosition.X + (Camera.Position.X - Screen.Width / 2),
+                    Input.MousePosition.Y + (Camera.Position.Y - Screen.Height / 2));
+            } else
+            {
+                if (!_disableCursorControllerMovement)
+                {
+                    var movementSpeedOrWhatever = 64;
+                    _cursorMovementDirection.X = _cursorMovementXAxisInput.Value;
+                    _cursorMovementDirection.Y = _cursorMovementYAxisInput.Value;
+
+                    if (_cursorMovementDirection.X > 0)
+                    {
+                        cursorEntity.Position = new Vector2(
+                            cursorEntity.Position.X + movementSpeedOrWhatever, 
+                            cursorEntity.Position.Y);
+                    }
+
+                    if (_cursorMovementDirection.X < 0)
+                    {
+                        cursorEntity.Position = new Vector2(
+                            cursorEntity.Position.X - movementSpeedOrWhatever, 
+                            cursorEntity.Position.Y);
+                    }
+
+                    if (_cursorMovementDirection.Y > 0)
+                    {
+                        cursorEntity.Position = new Vector2(
+                            cursorEntity.Position.X, 
+                            cursorEntity.Position.Y - movementSpeedOrWhatever);
+                    }
+
+                    if (_cursorMovementDirection.Y < 0)
+                    {
+                        cursorEntity.Position = new Vector2(
+                            cursorEntity.Position.X, 
+                            cursorEntity.Position.Y + movementSpeedOrWhatever);
+                    }
+
+                    if (_cursorMovementDirection.X != 0 || _cursorMovementDirection.Y != 0)
+                    {
+                        _disableCursorControllerMovement = true;
+
+                        _timer = new Timer(50);
+                        _timer.Enabled = true;
+                        _timer.Elapsed += (source, e) =>
+                        {
+                            _disableCursorControllerMovement = false;
+                            _timer.Enabled = false;
+                        };
+                    }
+                }
+
+                var bounds = Camera.Bounds;
+                var cursorEntitySpriteAnimator = cursorEntity.GetComponent<SpriteAnimator>();
+                if (cursorEntity.Position.X < bounds.X)
+                {
+                    cursorEntity.Position = new Vector2(
+                        bounds.X, 
+                        cursorEntity.Position.Y);
+                }
+
+                if (cursorEntity.Position.X + cursorEntitySpriteAnimator.Width > bounds.Right)
+                {
+                    cursorEntity.Position = new Vector2(
+                        bounds.Right - cursorEntitySpriteAnimator.Width, 
+                        cursorEntity.Position.Y);
+                }
+
+                if (cursorEntity.Position.Y < bounds.Top)
+                {
+                    cursorEntity.Position = new Vector2(
+                        cursorEntity.Position.X,
+                        bounds.Y);
+                }
+
+                if (cursorEntity.Position.Y + cursorEntitySpriteAnimator.Height > bounds.Bottom)
+                {
+                    cursorEntity.Position = new Vector2(
+                        cursorEntity.Position.X,
+                        bounds.Bottom - cursorEntitySpriteAnimator.Height);
+                }
+            }
+            // TODO: end system?
 
             _gridSystem.SnapEntitiesToGrid(EntitiesOfType<GridEntity>());
         }
@@ -107,6 +198,12 @@ namespace StratMono.Scenes
 
             cursorEntity.AddComponent(spriteAnimator);
             addToGrid(cursorEntity, 5, 13);
+
+            // TODO: consider moving elsewhere, see system comment in Update
+            _cursorMovementXAxisInput = new VirtualIntegerAxis();
+            _cursorMovementYAxisInput = new VirtualIntegerAxis();
+            _cursorMovementXAxisInput.Nodes.Add(new VirtualAxis.GamePadRightStickX());
+            _cursorMovementYAxisInput.Nodes.Add(new VirtualAxis.GamePadRightStickY());
         }
 
         private GridEntity addToGrid(GridEntity entity, int x, int y)
---
+++ StratMono/System/TileCursorSystem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nez.Systems;
+
+namespace StratMono.System
+{
+    class TileCursorSystem
+    {
+
+    }
+}
